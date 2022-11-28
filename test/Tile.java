3067500cf3668167cd5d267ad82b4f80567ace25
yuvalzadik <yuvalz@gthsoftware.com>
2022-11-28 14:51:30 +0200

    add equals and hash

--- test/Tile.java
+++ test/Tile.java
@@ -1,21 +1,28 @@
 package test.test;
+
+import java.util.Objects;
+
+//import  java.util.Objects ;
 public class Tile {
     public final char letter ;
     public final int score ;
 
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (!(object instanceof Tile tile)) return false;
-        if (!super.equals(object)) return false;
-        return letter == tile.letter && score == tile.score;
-    }
-
-    public int hashCode() {
-        return java.util.Objects.hash(super.hashCode(), letter, score);
-    }
-
     private Tile(char letter, int score) {
         this.letter = letter;
         this.score = score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null)
+            return false;
+        if (!(o instanceof Tile tile)) return false;
+        return letter == tile.letter && score == tile.score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(letter, score);
     }
 }
