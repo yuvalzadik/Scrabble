e0e7aa7082245f34ec1e26190c33f1478481e1a1
yuvalzadik <yuvalz@gthsoftware.com>
2022-11-28 18:09:28 +0200

    word class finish

--- test/Word.java
+++ test/Word.java
@@ -1,7 +1,43 @@
 package test.test;
 
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Word {
+    private Tile[] tiles;
+    private int row;
+    private int col;
+    private boolean vertical;
 
-	
+    public Word(Tile[] tiles, int row, int col, boolean vertical) {
+        this.tiles = tiles;
+        this.row = row;
+        this.col = col;
+        this.vertical = vertical;
+    }
+
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isVertical() {
+        return vertical;
+    }
+
+    @Override
+    // not the same
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Word word)) return false;
+        return getRow() == word.getRow() && getCol() == word.getCol() && isVertical() == word.isVertical() && Arrays.equals(getTiles(), word.getTiles());
+    }
+
 }
